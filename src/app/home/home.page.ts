f6288d6b0ec20730391ce194fa027a92eadba19b
soniajv-ionic <soniajv.ionic@gmail.com>
2025-01-21 13:58:00 +0100

    update 21/01/2025

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-
+import { IonInput, IonItem, IonList } from '@ionic/angular/standalone';
+import { IonButton } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonItem, IonList, IonButton, FormsModule],
 })
 export class HomePage {
+  altura:number | undefined;
+  peso:number | undefined;
+  imc: string | undefined;
   constructor() {}
+  calcularIMC() {
+    if (this.altura !== undefined && this.peso !== undefined) {
+      this.imc = (this.peso / Math.pow(this.altura, 2)).toFixed(6);
+    } else {
+    this.imc = "0.0";
+    }
+  }
 }
+
